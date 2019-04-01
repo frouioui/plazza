d1a26688f20a00ea8823518ff0426a76c00e1c68
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-01 16:06:44 +0200

    [ADD] - Travis file and its configuration

--- ci/script/deploy_epitech.sh
+++ ci/script/deploy_epitech.sh
@@ -4,4 +4,4 @@ cd $APPLICATION_DIRECTORY_DOCKER
 
 ssh-keyscan git.epitech.eu >> ~/.ssh/known_hosts
 git checkout $TRAVIS_BRANCH
-git push --repo=git@git.epitech.eu:/florent.poinsard@epitech.eu/OOP_nanotekspice_2018 --force
+git push --repo=git@git.epitech.eu:/florent.poinsard@epitech.eu/CCP_plazza_2018 --force

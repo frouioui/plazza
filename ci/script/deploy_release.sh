1a9fa38912e87c7fed33a0f2d7f625d359ee26aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-01 17:08:44 +0000

    Deploy frouioui/plazza to github.com/frouioui/plazza.git:gh-pages

--- ci/script/deploy_release.sh
+++ ci/script/deploy_release.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-cd $APPLICATION_DIRECTORY_DOCKER
-
-cd ..
-
 tar -czvf $ARCHIVE_NAME_RELEASE *
 
-mv $ARCHIVE_NAME_RELEASE $APPLICATION_DIRECTORY_DOCKER
+git config --local user.name "YOUR GIT USER NAME"
+git config --local user.email "YOUR GIT USER EMAIL"
+export TRAVIS_TAG=${TRAVIS_TAG:-"official"-$(date +'%d')/$(date +'%m')-$(date +'%H')h$(date +'%M')-$(git log --format=%h -1)}
+git tag $TRAVIS_TAG

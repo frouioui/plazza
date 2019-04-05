abe56057f9605cac1bcb2826618905dbefe93b10
Cécile <cecile.cadoul@epitech.eu>
2019-04-05 13:09:59 +0200

    [ADD] - Adding Kitchen .hpp and .cpp files

---
+++ include/Kitchen.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_plazza_2018
+** File description:
+** Kitchen class
+*/
+
+#ifndef KITCHEN_HPP_
+    #define KITCHEN_HPP_
+
+class Kitchen {
+    public:
+        Kitchen();
+        ~Kitchen();
+
+    private:
+};
+
+#endif /* !KITCHEN_HPP_ */

890306db692824b625e531b007185dff6d5a3347
Cécile <cecile.cadoul@epitech.eu>
2019-04-16 09:13:49 +0200

    [ADD] - First version of kitchen and cook classes

--- include/Kitchen/Cook.hpp
+++ include/Kitchen/Cook.hpp
@@ -8,35 +8,37 @@
 #ifndef COOK_HPP_
     #define COOK_HPP_
 
-    #include <list>
-    #include "Kitchen/Stock.hpp"
-    #include "Kitchen/SafeThread.hpp"
+#include <list>
+#include "Kitchen/Stock.hpp"
+#include "Kitchen/SafeThread.hpp"
 
-    namespace Kitchen {
+namespace Kitchen {
 
-        /**
-         * \class Cook
-         * \brief is the One Who Cook the Pizza
-         */
-        class Cook {
-            public:
-                /**
-                 * \brief Constructor of a Cook Who Need what he can do
-                 */
-                explicit Cook(SafeThread<Stock> &stock, SafeThread<std::list<Pizza::Pizza &>> &toDo, SafeThread<std::list<Pizza::Pizza &>> &finished);
+/**
+ * \class Cook
+ * \brief is the One Who Cook the Pizza
+ */
+    class Cook {
+        public:
+            /**
+             * \brief Constructor of a Cook Who Need what he can do
+             */
+            explicit Cook(SafeThread<Stock> &stock, SafeThread<std::list<Pizza::Pizza &>> &toDo, SafeThread<std::list<Pizza::Pizza &>> &finished);
 
-                /**
-                 * \brief DeConstructor
-                 */
-                ~Cook();
+            /**
+             * \brief DeConstructor
+             */
+            ~Cook();
 
-            protected:
-            private:
-                SafeThread<Stock> &_stock;
-                SafeThread<std::list<Pizza::Pizza *>> &_toDo;
-                SafeThread<std::list<Pizza::Pizza *>> &_finished;
-        };
+            Pizza::Pizza cookPizza();
 
+        protected:
+        private:
+            SafeThread<Stock> &_stock;
+            SafeThread<std::list<Pizza::Pizza *>> &_toDo;
+            SafeThread<std::list<Pizza::Pizza *>> &_finished;
     };
+
+};
 
 #endif /* !COOK_HPP_ */
--- include/Kitchen/Kitchen.hpp
+++ include/Kitchen/Kitchen.hpp
@@ -8,44 +8,84 @@
 #ifndef KITCHEN_HPP_
     #define KITCHEN_HPP_
 
-    #include <list>
-    #include "Pizza.hpp"
-    #include "Stock.hpp"
-    #include "SafeThread.hpp"
+#include <list>
+#include "Pizza.hpp"
+#include "Stock.hpp"
+#include "SafeThread.hpp"
+
+/**
+ * \namespace Kitchen
+ * \brief All elem that are necessary to a Kitchen
+ */
+namespace Kitchen {
 
     /**
-     * \namespace Kitchen
-     * \brief All elem that are necessary to a Kitchen
+     * \class Kitchen
+     * \brief Kitchen containt Cook that produce Pizza
      */
-    namespace Kitchen {
+    class Kitchen {
+        public:
 
-        /**
-         * \class Kitchen
-         * \brief Kitchen containt Cook that produce Pizza
-         */
-        class Kitchen {
-            public:
+            /**
+             * \brief Constructor
+             * \param[in] nbCooks --> Program Param
+             * \param[in] link between the reception and the Kitchen
+             */
+            Kitchen(const int nbCooks/*, TODO IPC IDENTIFIER*/);
 
-                /**
-                 * \brief Constructor
-                 * \param[in] nbCooks --> Program Param
-                 * \param[in] link between the reception and the Kitchen
-                 */
-                Kitchen(const int nbCooks/*TODO IPC IDENTIFIER*/);
+            ~Kitchen() = default;
+            /**
+             * \brief Display the kitchen status
+             *
+             * Display current occupancy of the cooks and
+             * theirs stocks of ingredient.
+             * This methods is called using the 'status' user command.
+             */
 
-                /**
-                 * Deconstructor
-                 */
-                ~Kitchen();
+            void displayStatus() const noexcept;
+            /**
+             * \brief Indicate if the current kitchen is saturated
+             *
+             * \return True if the kitchen cannot accept another pizza to cook
+             */
+            bool isSaturated() const noexcept;
 
-            private:
-                int _maxPizza;
-                SafeThread<std::list<Pizza::Pizza &>> _toDo;
-                SafeThread<std::list<Pizza::Pizza &>> _finished;
-                SafeThread<Stock> _stock;
-                /* TODO CONTAINER FOR THREADED COOK */
-        };
+            /**
+             * \brief send ready order to the reception
+             */
+            Pizza::Pizza sendReadyOrder(/*Must be determined*/);
+            /**
+             * \brief Add order to a list of pizza to cook
+             *
+             * \param pizza Pizza to cook
+             */
+            void addOrder(Pizza::Pizza pizza) noexcept;
 
+            /**
+             * \brief Assign a pizza to a free cooks
+             *
+             * Take the first pizza of the pizza list and assign it to a free cooks
+             * When a ready order is send to the reception, this method is called.
+             */
+            void assignPizza();
+
+        private:
+            SafeThread<std::list<Pizza::Pizza &>> _toDo;
+            SafeThread<std::list<Pizza::Pizza &>> _finished;
+
+            SafeThread<Stock> _stock;
+
+            int _maxPizza;
+            bool _saturated; /*!< True if kitchen is saturated, otherwise false */
+
+            // Timer occupation /*!< Kitchen must close after 5 sec of inactivity*/
+
+            /**
+             * \brief Calculate saturation indicator
+             */
+            void calculStaturation() noexcept;
     };
+
+};
 
 #endif /* !KITCHEN_HPP_ */

8e9806e402d64dd3dcca79a1e542fca3bce23c7f
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-18 08:24:45 +0200

    [MOD] - Moficiation of the string library unit test (#53)

--- lib/string_parser/src/StringParser.cpp
+++ lib/string_parser/src/StringParser.cpp
@@ -17,6 +17,23 @@ StringParser::~StringParser()
 {
 }
 
+std::string ltrim(const std::string& s)
+{
+    size_t start = s.find_first_not_of(" \t");
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+
+std::string rtrim(const std::string& s)
+{
+    size_t end = s.find_last_not_of(" \n");
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+
+std::string trim(const std::string& s)
+{
+    return rtrim(ltrim(s));
+}
+
 const std::string &StringParser::getStr() const noexcept
 {
     return _str;
@@ -51,6 +68,7 @@ void StringParser::removeSpaceAndTabs() noexcept
 {
     unsigned int j = 0;
 
+    _str = trim(_str);
     for (unsigned int i = 0; i < _str.size(); i++) {
         if ((_str[i] == ' ' || _str[i] == '\t') && (_str[i + 1] == ' ' || _str[i + 1] == '\t')) {
             i++;
